810da5185ba2b70589d57a2f493ac47075f2e6fe
muddafudda <muddafudda@users.noreply.github.com>
2013-10-31 00:53:51 +0800

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -21,6 +21,7 @@ static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
     ( 0, 0x0e00670bu )
     ( 131315, 0xe17f2a80u )
+    ( 184030, 0x2b849b34u )
     ;
 
 // Get the last stake modifier and its generation time from a given block
